87a29eeffb46eb84f2f01eff6e7fe2442982e792
Найманов Егор Викторович <is742s13@mwp11.csc.local>
2018-04-25 15:02:15 +0700

    Main reload v2

--- main.c
+++ main.c
@@ -29,3 +29,36 @@ int main() {
 char places[6];
 int exitt = 0;
 
+int board_main() {
+    system("clear");
+    print_new_board();
+    
+    while (exitt == 0) {
+        help_print();
+        printf("Введите координаты перехода \n."
+        "Пример a2-c8\n""\t\t%sCommand:\n\t\t ", GREEN);
+        scanf("%s", places);
+        printf("%s", RESET);
+        if (places[0] == 'r') {
+            system("clear");
+            exitt = 1;
+            board_main();
+        } else if (places[0] == 'q') {
+            system("clear");
+            printf("%sGoodbye!%s\n",BLACK, RESET);
+            return 0;
+        }
+        system("clear");
+        exitt = board_func(places);
+        if (exitt == -1) {
+            print_board();
+            if (places[0] == 'r') {
+                printf("%s\t\tRestart!\n%s", GREEN, RESET);
+            } else {
+                printf("%s\tНеправильные координаты\n%s", RED, RESET);
+            }
+            exitt = 0;
+        }
+    }
+    return 0;
+}
